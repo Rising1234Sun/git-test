edc1c087dd7cf60bb748597245c376fa34a60751
Rising123Sun <rising123sun@gmail.com>
2019-10-23 20:33:47 +0800

    第一次提交到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,20 @@
+package com.itheima.git;
+
+/**
+ * @ClassName: FirstClass
+ * @Description: TODO
+ * @Author: Risin
+ * @date: 2019/10/23 20:31
+ * @Version: 1:0
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
